21f29d58f9f9a57bf5b11e3fe015e8c854f00f3f
BenteG <bente.geerts@student.kdg.be>
2022-03-12 09:56:10 +0100

    added postscontroller routes to routes file

--- blog-laravel/routes/web.php
+++ blog-laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\PageController;
 */
 
 Route::get('/', [PageController::class, "index"]);
+Route::resource("Post", PostsController::class);
