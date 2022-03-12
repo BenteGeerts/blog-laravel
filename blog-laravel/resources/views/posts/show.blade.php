42ff88b8db61d4734fb1147c7ab0c4dedd6bdd6a
BenteG <bente.geerts@student.kdg.be>
2022-03-12 19:38:02 +0100

    added remove button and functionality

--- blog-laravel/resources/views/posts/show.blade.php
+++ blog-laravel/resources/views/posts/show.blade.php
@@ -1,12 +1,17 @@
 @extends("layouts.app")
 
 @section("content")
+    <a href="/posts" class="btn btn-primary">Go back</a>
     <h1>{{$post->title}}</h1>
     <small>Written on {{$post->created_at}}</small>
     <div>
         {{$post->content}}
     </div>
-    <a href="/posts" class="btn btn-primary">Go back</a>
-    <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+    <hr>
+    <form method="post" action="{{route("posts.destroy", $post->id)}}">
+        @csrf
+        <input type="hidden" value="delete" name="_method">
+        <button class="btn btn-danger">Delete</button>
+    </form>
 @endsection
