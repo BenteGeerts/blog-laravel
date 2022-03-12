e877e058e73743aa5485f2a293b194cb4c18a213
BenteG <bente.geerts@student.kdg.be>
2022-03-12 11:51:07 +0100

    created frontend for create form

--- blog-laravel/resources/views/posts/create.blade.php
+++ blog-laravel/resources/views/posts/create.blade.php
@@ -1,1 +1,17 @@
-<?php
+@extends("layouts.app")
+
+@section("content")
+    <h1>Create post</h1>
+    <form>
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" placeholder="Enter title">
+        </div>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea class="form-control" placeholder="Content" rows="10"></textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
