6cf76257b8ab229b1b50fac1804b0ad4095b9371
BenteG <bente.geerts@student.kdg.be>
2022-03-23 13:25:39 +0100

    finished development

--- blog-laravel/resources/views/index.blade.php
+++ blog-laravel/resources/views/index.blade.php
@@ -2,9 +2,9 @@
 
 @section("content")
     <div class="px-4 py-5 my-5 text-center border">
-        <h1 class="display-5 fw-bold">Welcome</h1>
+        <h1 class="display-5 fw-bold">Hi</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <p class="lead mb-4">Welcome to Bente's blog</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <a href="/login" type="button" class="btn btn-primary btn-lg px-4 gap-3">Login</a>
                 <a href="/register" type="button" class="btn btn-primary btn-lg px-4">Register</a>
